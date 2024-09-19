b0137bc6facbec98d65846b46bd1c17454fd6d5d
John Dunn <John.Dunn@qsc.com>
2024-09-19 14:52:11 -0600

    cleanup:

--- src/App.js
+++ src/App.js
@@ -12,19 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={doit}>hey</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={doit}>Click Me</button>
       </header>
     </div>
   );
